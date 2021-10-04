153583bff17382c68c8e9e1fbea3939dc1c96fd2
Iago Baldani <iagobaldanialmeida@hotmail.com>
2021-10-04 09:03:15 -0300

    Script Json - Incompleto

--- Dados do Programa de Formação - Cadastro e Edição/js/scriptget.js
+++ Dados do Programa de Formação - Cadastro e Edição/js/scriptget.js
@@ -1,5 +1,4 @@
 function onLoad() {
-
     var app = new Vue({
         el: '#coordenadores',
         data: {
@@ -16,24 +15,53 @@ function onLoad() {
         }
         
     })
-
-    var app2 = new Vue({
-        el: '#formulario',
-        data: {
-            programa:{
-                "nome" : "",
-                "inicio": "",
-                "termino": "",
-                "coordenador": 0,
-                "turma": ""
-            }
-        },
-        methods:{
-         
-        }
-    })
-
-
 }
 
+function main(){
+    dados = salvaDados();
+    carregaDados(dados);
+}
+
+function carregaDados(dados){
+    let dadosExibicao = dados;
+    
+    dadosExibicao.inicio = formataData(dados.inicio);
+    dadosExibicao.termino = formataData(dados.termino);
+
+    var app2 = new Vue({
+        el: '#programa-modal',
+        data: {
+            programa:{
+                'nome': dadosExibicao.nome,
+                'inicio': dadosExibicao.inicio,
+                'termino': dadosExibicao.termino,
+                'coordenador': dadosExibicao.coordenador,
+                'turma': dadosExibicao.turma
+            }
+        }
+    }) 
+}
+
+function salvaDados(){
+    var dados = {
+        'nome': getValueById("nome"),
+        'inicio': getValueById("inicio"),
+        'termino': getValueById("termino"),
+        'coordenador': getValueById("coordenador"),
+        'turma': getValueById("turma")
+    }
+
+    return dados;
+}
+
+function getValueById(id){
+   return document.getElementById(id).value;
+}
+
+function formataData(data){
+    let dataPreForm = new Date(data);
+    let dataFormatada = `${dataPreForm.getDate()}/${dataPreForm.getMonth() + 1}/${dataPreForm.getFullYear()}`
+    
+    return dataFormatada;
+}
 
